242f8c549b6c4dbc630e53823399512d891de53f
Cerylion <fullthrottle.moran@gmail.com>
2024-04-02 19:12:34 -0600

    clase 01 primera parte

---
+++ clase_01/Actividad_clase_01.js
@@ -0,0 +1,11 @@
+// Primera actividad
+// programa que guarde número de órdenes
+// determine si las órdenes superaron el monto previsto
+// o si las ordenes fueron menores de lo previsto
+// guardar dia del mes en el programa
+// aviso dias impar con demanda mayor a la prevista
+
+const ordenMinDiario = 8
+let orden = 0
+const ordenHoy = orden
+
---
+++ clase_01/clase_01.js
@@ -0,0 +1,11 @@
+function salute() {
+  console.log('Hello Koders!')
+}
+
+salute()
+
+function salute2(name) {
+  console.log(`Hello ${name}!`)
+}
+
+salute2('Manuel')
